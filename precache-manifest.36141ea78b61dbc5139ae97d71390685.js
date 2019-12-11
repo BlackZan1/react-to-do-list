576768d14df923818fa80c6f3446f1d2e6283fc3
BlackZan1 <kenshin.nakamura.01@mail.ru>
2019-12-11 16:31:37 +0300

    Updates

---
+++ precache-manifest.36141ea78b61dbc5139ae97d71390685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40079539c159965816e27e87bb06a8a",
+    "url": "/NazarsToDOList/index.html"
+  },
+  {
+    "revision": "4bac911beb032553301e",
+    "url": "/NazarsToDOList/static/css/main.519ad0ab.chunk.css"
+  },
+  {
+    "revision": "b7e36d81322c99074482",
+    "url": "/NazarsToDOList/static/js/2.188229b4.chunk.js"
+  },
+  {
+    "revision": "53edb15dd7798d69c564",
+    "url": "/NazarsToDOList/static/js/3.6a8fa88c.chunk.js"
+  },
+  {
+    "revision": "4bac911beb032553301e",
+    "url": "/NazarsToDOList/static/js/main.08c12b3a.chunk.js"
+  },
+  {
+    "revision": "abdbd5f63a2bb704ce13",
+    "url": "/NazarsToDOList/static/js/runtime-main.678c5cad.js"
+  }
+]);
