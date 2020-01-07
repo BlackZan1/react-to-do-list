a601a00d739bc810eb4d692e9c8696459b22519e
BlackZan1 <kenshin.nakamura.01@mail.ru>
2020-01-07 16:03:34 +0300

    Updates

---
+++ precache-manifest.68afb1cd820dd9e7bc6b3f29385f4a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138adaf52419a78bb9139418743e5f6c",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "2d0f01df0fbd2a03f743",
+    "url": "/react-to-do-list/static/css/main.519ad0ab.chunk.css"
+  },
+  {
+    "revision": "b7e36d81322c99074482",
+    "url": "/react-to-do-list/static/js/2.188229b4.chunk.js"
+  },
+  {
+    "revision": "64f771680cb671fb454d",
+    "url": "/react-to-do-list/static/js/3.78c6e52e.chunk.js"
+  },
+  {
+    "revision": "2d0f01df0fbd2a03f743",
+    "url": "/react-to-do-list/static/js/main.925ec8a4.chunk.js"
+  },
+  {
+    "revision": "5082558d420b307965d4",
+    "url": "/react-to-do-list/static/js/runtime-main.f7d3e406.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NazarsToDOList/precache-manifest.36141ea78b61dbc5139ae97d71390685.js"
+  "/react-to-do-list/precache-manifest.68afb1cd820dd9e7bc6b3f29385f4a5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NazarsToDOList/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-to-do-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
